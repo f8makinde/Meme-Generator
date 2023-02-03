adfb26370a4e46b11deed9b1ddc0501b092f5b08
Faith Makinde <faithmakinde0@gmail.com>
2023-02-03 16:00:12 +0100

    Main section completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Nav from '../components/Nav.js'
-
+import Main from '../components/Main.js'
 export default function App(){
     return (
         <>
             <Nav />
+            <Main />
         </>
     )
 }
